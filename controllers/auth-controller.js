32b73ddbeb2ca6bcf6ea3167aed58bf1ed8ae553
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-24 22:02:35 +0500

    added login

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,12 +1,13 @@
 const { read_file, write_file } = require("../fs/fs-api");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const usersFile = "users.json";
 
 const Auth = {
   REGISTER: async (req, res) => {
     const { email, password, username } = req.body;
     const allUsers = read_file(usersFile);
-    const checkUser = allUsers.find((user) => user.email == email);
+    const checkUser = allUsers.find((user) => user.email === email);
     const hashPassword = await bcrypt.hash(password, 12);
 
     if (checkUser) {
@@ -27,8 +28,25 @@ const Auth = {
   },
 
   LOGIN: async (req, res) => {
-    
-  }
+    const { email, password } = req.body;
+    const checkUser = read_file(usersFile).find((user) => user.email === email);
+    if (!checkUser) {
+      return res.status(404).send({ message: "User not found!" });
+    }
+
+    const checkPassword = await bcrypt.compare(password, checkUser.password);
+
+    if (!checkPassword)
+      return res.status(401).send({ message: "Invalid Password!" });
+
+    const token = jwt.sign(
+      { id: checkUser.id, email: checkUser.email },
+      process.env.SECRET_KEY,
+      { expiresIn: "10s" }
+    );
+
+    res.status(200).send({ token: token });
+  },
 };
 
 module.exports = Auth;
